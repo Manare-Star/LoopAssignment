4dbe5b6850bdca117b15f539e3d630de5ec03d38
Manare <manarestarboy@icloud.com>
2024-09-26 10:08:55 +0200

    Add project files.

---
+++ LoopAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LoopAssignment
+{
+    internal class Program
+    {
+        class BeerSong
+        {
+            public void sing() 
+            {
+
+                for (int i = 99; i > 0; i--)
+                {
+                    Console.WriteLine($"{i} bottles of beer on the wall, {i} bottles of beer, Take one down and pass it around,");
+                }
+            }
+               
+               
+        }
+        static void Main(string[] args)
+        {
+            BeerSong star = new BeerSong();
+            star.sing();
+        }
+    }
+}
